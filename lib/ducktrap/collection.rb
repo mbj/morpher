b24dec8e4a2f3da84cf87f51a7559acc21d73123
Markus Schirp <mbj@seonic.net>
2012-11-18 17:30:07 +0100

    Add docs and equalization for collection

--- lib/ducktrap/collection.rb
+++ lib/ducktrap/collection.rb
@@ -1,17 +1,31 @@
 class Ducktrap
+  # Ducktrap that applies operand over collection
   class Collection < self
-    include Unary
+    include Unary, Equalizer.new(:operand)
 
     register :collection
 
+    # Return inverse
+    #
+    # @return [Ducktrap]
+    #
+    # @api private
+    #
     def inverse
       self.class.new(operand.inverse)
     end
 
+    # Result for collection ducktrap
     class Result < Unary::Result
 
     private
 
+      # Calculate result
+      #
+      # @return [Object]
+      #
+      # @api private
+      #
       def process
         input.map do |element|
           result = operand.run(element)
