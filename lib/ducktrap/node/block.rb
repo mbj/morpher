c43c172effe966bade582aa30b1b9482cf74c80a
Markus Schirp <mbj@schirp-dso.com>
2013-05-25 22:00:29 +0200

    style

--- lib/ducktrap/node/block.rb
+++ lib/ducktrap/node/block.rb
@@ -33,15 +33,12 @@ module Ducktrap
         def process
           body.inject(input) do |input, ducktrap|
             evaluator = ducktrap.run(input)
-
-            unless evaluator.successful?
-              return nested_error(evaluator)
-            end
-
+            return nested_error(evaluator) unless evaluator.successful?
             evaluator.output
           end
         end
-      end
-    end
-  end
-end
+
+      end # Evaluator
+    end # Block
+  end # Node
+end # Ducktrap
