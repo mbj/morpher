f537829276df91b5d50413efee69ff56a6d42f0a
Markus Schirp <mbj@schirp-dso.com>
2013-05-25 13:31:01 +0200

    Kill mutations in Ducktrap::Error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,19 @@ require 'ducktrap'
 require 'devtools'
 Devtools.init_spec_helper
 
+module StripHelper
+  def strip(string)
+    lines = string.lines
+    line = lines.first
+    match = /\A[ ]*/.match(line)
+    length = match[0].length
+    source = lines.map do |line|
+      line[(length..-1)]
+    end.join
+  end
+end
+
 RSpec.configure do |config|
   config.extend(LetMockHelper)
+  config.include(StripHelper)
 end
