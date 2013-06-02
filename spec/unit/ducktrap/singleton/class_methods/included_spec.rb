48da44cab799397b5d4fbcee5a7c7e516f86df0e
Markus Schirp <mbj@schirp-dso.com>
2013-06-02 17:28:05 +0200

    Workaround bug in rbx
    Tested under 2.0.0.rc1
    
    ```
    class Foo; private_class_method :new; end
    Foo.public_methods.include?(:new) => true # rbx
    Foo.public_methods.include?(:new) => false # mri
    ```
    
    `Foo.new` will raise in both cases.

--- spec/unit/ducktrap/singleton/class_methods/included_spec.rb
+++ spec/unit/ducktrap/singleton/class_methods/included_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe Ducktrap::Singleton, '.included' do
   subject { described_class = self.described_class; Class.new { include described_class } }
 
-  its(:ancestors) { should include(Ducktrap::Singleton::InstanceMethods) }
+  its(:ancestors)      { should include(Ducktrap::Singleton::InstanceMethods) }
+  
+  it 'should not be possible to create instance via .new' do
+    expect { subject.new }.to raise_error
+  end
+
   it { should respond_to(:instance) }
-  its(:public_methods) { should_not include(:new) }
 end
