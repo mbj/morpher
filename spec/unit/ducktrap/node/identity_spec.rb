bda07919115e494f759e1581b13faa1099e544b6
Markus Schirp <mbj@schirp-dso.com>
2013-11-12 19:14:35 +0100

    Add Ducktrap::Node::Identity

---
+++ spec/unit/ducktrap/node/identity_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Ducktrap::Node::Identity do
+
+  let(:object) { described_class.new }
+
+  describe '#call' do
+    subject { object.call(input) }
+
+    let(:input) { double('Input') }
+
+    it { should eql(Ducktrap::Evaluator::Noop.new(object, input)) }
+  end
+
+  describe '#inverse' do
+    subject { object.inverse }
+
+    it { should be(object) }
+  end
+
+  describe '#pretty_inspect' do
+    subject { object.pretty_inspect }
+
+    it { should eql("Ducktrap::Node::Identity\n") }
+  end
+end
