1cd15a1414069f670b291e8cd77e1dfe9d26eb5a
Markus Schirp <mbj@schirp-dso.com>
2013-12-01 17:50:53 +0100

    Kill mutations in Evaluator.register

---
+++ spec/unit/morpher/evaluator_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Morpher::Evaluator do
+  describe '.register' do
+    let(:object) do
+      Class.new(described_class) do
+        public_class_method :register
+      end
+    end
+
+    subject { object.register(:foo) }
+
+    it 'registers evaluator' do
+      expect { subject }.to change { Morpher::REGISTRY[:foo] }.from(nil).to(object)
+    end
+  end
+end
