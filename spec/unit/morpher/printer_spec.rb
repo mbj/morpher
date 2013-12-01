130c87ebae1836be8c64ecd22ca09485b4bb94fe
Markus Schirp <mbj@schirp-dso.com>
2013-12-01 18:10:30 +0100

    Add uncommited files from prev commit

---
+++ spec/unit/morpher/printer_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Morpher::Printer::Mixin do
+  describe '#description' do
+    let(:object) do
+      Class.new do
+        include Morpher::Printer::Mixin, Adamantium::Flat
+
+        def self.name
+          'Foo'
+        end
+
+        printer do
+          name
+        end
+      end
+    end
+
+    subject { object.new.description }
+
+    it { should eql("Foo\n") }
+  end
+end
