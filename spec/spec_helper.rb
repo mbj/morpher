31b827f1540bc6f90d2be20e449632e3b9ee91f6
Markus Schirp <mbj@schirp-dso.com>
2013-12-01 18:04:17 +0100

    Add monkeypatch for full implicit coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,11 @@ require 'mutant' # for the node helpers
 
 # Monkeypatch to mutant .rc3 fixing double diffs error.
 #
+# Also does run all mutations.
+#
 # TODO: Use master once it supports configurable implicit coverage.
+#
+# Morpher predicates are needed to finally make this configurable in mutant.
 #
 module Mutant
   class Subject
@@ -48,7 +52,23 @@ module Mutant
       end
     end
   end
-end
+
+  class Killer
+    class Rspec
+
+      # Return all example groups
+      #
+      # @return [Enumerable<RSpec::Example>]
+      #
+      # @api private
+      #
+      def example_groups
+        strategy.example_groups
+      end
+
+    end # Rspec
+  end # Killer
+end # Mutant
 
 RSpec.configure do |config|
   config.expect_with :rspec do |rspec|
