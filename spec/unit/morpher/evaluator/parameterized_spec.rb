c05976b6be7fb1c8b6b80494820321900bdc8b54
Markus Schirp <mbj@schirp-dso.com>
2013-12-02 12:09:45 +0100

    Kill mutations in Morpher::Evaluator::Parameterized

---
+++ spec/unit/morpher/evaluator/parameterized_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Morpher::Evaluator::Parameterized do
+
+  describe '.include' do
+    let(:object) do
+      Class.new(Morpher::Evaluator) do
+        include Morpher::Evaluator::Parameterized
+
+        def self.name
+          'Foo'
+        end
+      end
+    end
+
+    let(:instance) do
+      object.new('foo')
+    end
+
+    it 'sets up printer' do
+      instance.description.should eql(strip(<<-'TEXT'))
+        Foo
+          param: "foo"
+      TEXT
+    end
+
+    it 'sets up .build' do
+      expect(object.build(Morpher::Compiler.new(Morpher::REGISTRY), s(:foo, "foo"))).to eql(instance)
+    end
+  end
+end
