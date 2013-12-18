28923664b62b17f4a341ee10f4281fa03c730b70
Markus Schirp <mbj@schirp-dso.com>
2013-12-18 22:36:46 +0100

    Kill mutations in Morpher::Registry
    Currently this lib is mutation covered via implicit coverage. Later I'll
    move to explicit one!

---
+++ spec/unit/morpher/registry_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Morpher::Registry do
+
+  specify do
+    klass = Class.new do
+      include Morpher::Registry
+      register :foo
+    end
+
+    expect(klass::REGISTRY).to eql({:foo => klass})
+  end
+end
