c233ae51646bb141028ffe056b088c06c5ae687d
Markus Schirp <mbj@schirp-dso.com>
2014-01-15 20:29:16 +0100

    Adjust specs for unary predicate evaluators

--- spec/unit/morpher/evaluator/predicate/unary_spec.rb
+++ spec/unit/morpher/evaluator/predicate/unary_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Morpher::Evaluator::Predicate::Unary::Negation do
   let(:object) { described_class.new(Morpher::Evaluator::Predicate::EQL.new('foo')) }
 
-  let(:valid_input)   { 'bar' }
-  let(:invalid_input) { 'foo' }
+  let(:positive_input) { 'bar' }
+  let(:negative_input) { 'foo' }
 
   include_examples 'predicate evaluator'
 
