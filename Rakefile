cdd0fbab7e81e521da756e268658068785ef73e1
Markus Schirp <mbj@schirp-dso.com>
2014-03-01 03:43:52 +0000

    Add example

--- Rakefile
+++ Rakefile
@@ -3,6 +3,14 @@ Devtools.init_rake_tasks
 
 namespace :metrics do
   task :mutant => :coverage do
+  end
+end
+
+Rake.application.load_imports
+task('metrics:mutant').clear
+
+namespace :metrics do
+  task :mutant => :coverage do
     $stderr.puts 'Mutant on CI is curently disable for this repo'
   end
 end
---
+++ example.rb
@@ -0,0 +1,34 @@
+require 'morpher'
+extend Morpher::NodeHelpers
+
+class Address
+  include Anima.new(:street)
+end
+
+class Person
+  include Anima.new(:address)
+end
+
+node = s(:block,
+  s(:guard, s(:primitive, Hash)),
+  s(:hash_transform,
+    s(:symbolize_key, 'street',
+      s(:guard, s(:primitive, String))
+    )
+  ),
+  s(:anima_load, Address)
+)
+
+ADDRESS_EVALUATOR = Morpher.evaluator(node)
+
+node = s(:block,
+  s(:guard, s(:primitive, Hash)),
+  s(:hash_transform,
+    s(:symbolize_key, 'address',
+      ADDRESS_EVALUATOR.node
+    )
+  ),
+  s(:anima_load, Person)
+)
+
+PERSON_EVALUATOR = Morpher.evaluator(node)
