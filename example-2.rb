4beef794d8b8e241a2e447b5fe3380789828b812
Markus Schirp <mbj@schirp-dso.com>
2014-03-05 14:37:01 +0000

    Add other example I used for pairing

---
+++ example-2.rb
@@ -0,0 +1,27 @@
+require 'morpher'
+
+extend Morpher::NodeHelpers
+
+class Input
+  include Anima.new(:foo)
+end # Input
+
+# Input.new(:foo => 'bar')
+
+node =
+  s(:block,
+    s(:guard, s(:primitive, Hash)),
+    s(:hash_transform,
+      s(:symbolize_key, :foo,
+        s(:guard,
+          s(:or,
+            s(:primitive, String),
+            s(:primitive, NilClass)
+          )
+        )
+      )
+    ),
+    s(:anima_load, Input)
+  )
+
+EVALUATOR = Morpher.evaluator(node)
