0d925bbeb3add41dab91d5cb99aafca398e3c3b1
Markus Schirp <mbj@schirp-dso.com>
2014-03-08 20:03:37 +0000

    Move key preprocessors into namespace

--- lib/morpher/preprocessor/emitter.rb
+++ lib/morpher/preprocessor/emitter.rb
@@ -65,49 +65,6 @@ module Morpher
         end
 
       end # Noop
-
-      # Key symbolization preprocessor
-      class KeySymbolize < self
-
-        register :key_symbolize
-
-        # Return transformed node
-        #
-        # @param [Node] node
-        #
-        # @return [Node]
-        #
-        # @api private
-        #
-        def output
-          key, operand = *node
-          s(:key_transform, key.to_s, key.to_sym, visit(operand))
-        end
-
-      end
-
-      # Key transformation preprocessor
-      class KeyTransform < self
-        register :key_transform
-
-        # Return transformed node
-        #
-        # @param [Node] node
-        #
-        # @return [Node]
-        #
-        # @api private
-        #
-        def output
-          from, to, operand = *node
-          s(:block,
-            s(:key_fetch, from),
-            visit(operand),
-            s(:key_dump, to)
-          )
-        end
-
-      end # KeyTransform
     end # Emitter
   end # Preprocessor
 end # Morpher
